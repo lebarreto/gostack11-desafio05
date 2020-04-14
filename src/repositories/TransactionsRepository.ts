bd69119e39ffd8bf2f3c6f700d269c47c5412d64
leticialmb <leticiabarreto196@gmail.com>
2020-04-14 11:39:49 -0400

    Desafio 05

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,32 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    var income = 0;
+    var outcome = 0;
+
+    const total = this.transactions.reduce((accumulator, currentValue) => {
+      if (currentValue.type === 'income') {
+        income += currentValue.value;
+        return accumulator + currentValue.value;
+      }
+      else {
+        outcome += currentValue.value;
+        return accumulator - currentValue.value;
+      }
+    }, 0);
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
